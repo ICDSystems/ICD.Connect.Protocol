42fe113db995aab160449267bf077d37087fb08e
Drew Tingen <drew.tingen@wedoresi.com>
2017-07-25 01:03:34 -0400

    Renamed SimplPlus Crosspoints to CrosspointWrapper Added ISimplPlusCrosspointWrapper interface Added SimplPlusCrosspointWrapperManager to provide a console interface to the SimplPlus wrappers

---
+++ ICD.Connect.Protocol.Crosspoints/SimplPlus/CrosspointWrappers/ISimplPlusCrosspointWrapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ICD.Connect.Protocol.Crosspoints.Crosspoints;
+
+namespace ICD.Connect.Protocol.Crosspoints.SimplPlus.CrosspointWrappers
+{
+	public interface ISimplPlusCrosspointWrapper
+	{
+
+		#region Properties
+
+		int SystemId { get; }
+
+		int CrosspointId { get; }
+
+		string CrosspointName { get; }
+
+		string CrosspointSymbolInstanceName { get; }
+
+		ICrosspoint Crosspoint { get; }
+
+		#endregion
+
+		#region Events
+
+		event EventHandler OnCrosspointChanged;
+
+		#endregion
+
+		#region Methods
+
+		string GetWrapperInfo();
+
+		#endregion
+	}
+}
