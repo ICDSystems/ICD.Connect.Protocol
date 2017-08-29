c88cbc00c8ca2d5540617480063e9ec32f41096a
Chris Cameron <chris.cameron@wedoresi.com>
2017-08-29 16:04:00 -0400

    Tidying DirectMessageManager, more exhaustive disposal of dependencies

--- ICD.Connect.Protocol.Network/Direct/DirectMessageManager.cs
+++ ICD.Connect.Protocol.Network/Direct/DirectMessageManager.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using ICD.Common.Properties;
 using ICD.Common.Utils;
+using ICD.Common.Utils.Json;
 using ICD.Connect.Protocol.Network.Tcp;
 using ICD.Connect.Protocol.Network.Utils;
 using ICD.Connect.Protocol.Ports;
 using ICD.Connect.Protocol.SerialBuffers;
-using Newtonsoft.Json;
 
 namespace ICD.Connect.Protocol.Network.Direct
 {
@@ -16,21 +17,26 @@ namespace ICD.Connect.Protocol.Network.Direct
 
 	public sealed class DirectMessageManager : IDisposable
 	{
-		// stores clients and buffers so they do not get garbage collected
 		private readonly TcpClientPool m_ClientPool;
 		private readonly TcpClientPoolBufferManager m_ClientBuffers;
+
+		private readonly AsyncTcpServer m_Server;
+		private readonly TcpServerBufferManager m_ServerBuffer;
 
 		private readonly Dictionary<Guid, ClientBufferCallback> m_MessageCallbacks;
 		private readonly Dictionary<Type, IMessageHandler> m_MessageHandlers;
 		private readonly SafeCriticalSection m_MessageHandlersSection;
-		private readonly AsyncTcpServer m_Server;
-		private readonly TcpServerBufferManager m_ServerBuffer;
+
 		private readonly int m_SystemId;
+
+		#region Constructors
 
 		/// <summary>
 		/// Creates a DirectMessageManager with default systemId of 0
 		/// </summary>
-		public DirectMessageManager() : this(0)
+		[PublicAPI]
+		public DirectMessageManager()
+			: this(0)
 		{
 		}
 
@@ -39,92 +45,97 @@ namespace ICD.Connect.Protocol.Network.Direct
 		/// DirectMessageManagers on different system IDs cannot communicate with each other.
 		/// </summary>
 		/// <param name="systemId"></param>
+		[PublicAPI]
 		public DirectMessageManager(int systemId)
 		{
-			m_SystemId = systemId;
-			m_Server = new AsyncTcpServer(NetworkUtils.GetDirectMessagePortForSystem(systemId), 64);
+			m_MessageHandlers = new Dictionary<Type, IMessageHandler>();
+			m_MessageCallbacks = new Dictionary<Guid, ClientBufferCallback>();
+			m_MessageHandlersSection = new SafeCriticalSection();
 
+			m_SystemId = systemId;
+
+			m_Server = new AsyncTcpServer(NetworkUtils.GetDirectMessagePortForSystem(systemId), 64);
 			m_ServerBuffer = new TcpServerBufferManager(() => new DelimiterSerialBuffer(AbstractMessage.DELIMITER));
 			m_ServerBuffer.SetServer(m_Server);
-			m_ServerBuffer.OnClientCompletedSerial += ServerBufferOnClientCompletedSerial;
-
-			m_MessageHandlers = new Dictionary<Type, IMessageHandler>();
-			m_MessageHandlersSection = new SafeCriticalSection();
+			Subscribe(m_ServerBuffer);
 
 			m_ClientPool = new TcpClientPool();
 			m_ClientBuffers = new TcpClientPoolBufferManager(() => new DelimiterSerialBuffer(AbstractMessage.DELIMITER));
 			m_ClientBuffers.SetPool(m_ClientPool);
-			m_ClientBuffers.OnClientCompletedSerial += ClientPoolBufferOnClientCompletedSerial;
-
-			m_MessageCallbacks = new Dictionary<Guid, ClientBufferCallback>();
+			Subscribe(m_ClientBuffers);
 
 			m_Server.Start();
 		}
 
-		private void ServerBufferOnClientCompletedSerial(TcpServerBufferManager sender, uint clientId, string data)
+		#endregion
+
+		/// <summary>
+		/// Release resources.
+		/// </summary>
+		public void Dispose()
 		{
-			AbstractMessage msg = AbstractMessage.Deserialize(data);
-			if (msg == null)
-				return;
-			msg.ClientId = clientId;
-			Type type = msg.GetType();
-			if (m_MessageHandlers.ContainsKey(type))
-			{
-				AbstractMessage response = m_MessageHandlers[type].HandleMessage(msg);
-				if (response != null && m_Server.ClientConnected(clientId))
-				{
-					response.MessageId = msg.MessageId;
-					response.MessageFrom = GetHostInfo();
-					m_Server.Send(clientId, response.Serialize());
-				}
-			}
+			Unsubscribe(m_ServerBuffer);
+			m_ServerBuffer.Dispose();
+
+			Unsubscribe(m_ClientBuffers);
+			m_ClientBuffers.Dispose();
+
+			m_MessageCallbacks.Clear();
+
+			foreach (IDisposable handler in m_MessageHandlers.Values.OfType<IDisposable>())
+				handler.Dispose();
+			m_MessageHandlers.Clear();
+
+			m_Server.Dispose();
+			m_ClientPool.Dispose();
 		}
 
 		#region Server Methods
 
+		/// <summary>
+		/// Adds the given message handler to the manager.
+		/// </summary>
+		/// <typeparam name="TMessage"></typeparam>
+		/// <param name="handler"></param>
 		public void RegisterMessageHandler<TMessage>(AbstractMessageHandler<TMessage> handler)
 			where TMessage : AbstractMessage, new()
 		{
-			Type t = typeof(TMessage);
-			m_MessageHandlersSection.Enter();
-			if (!m_MessageHandlers.ContainsKey(t))
-				m_MessageHandlers.Add(t, handler);
-			else
-				m_MessageHandlers[t] = handler;
-
-			// cache serialization info
-			string serialized = JsonConvert.SerializeObject(ReflectionUtils.CreateInstance<TMessage>());
-			JsonConvert.DeserializeObject<TMessage>(serialized);
-
-			m_MessageHandlersSection.Leave();
+			m_MessageHandlersSection.Execute(() => m_MessageHandlers[typeof(TMessage)] = handler);
+			JsonUtils.CacheType<TMessage>();
 		}
 
+		/// <summary>
+		/// Removes the message handler of the given type from the manager.
+		/// </summary>
+		/// <typeparam name="TMessage"></typeparam>
 		public void UnregisterMessageHandler<TMessage>()
+			where TMessage : AbstractMessage, new()
 		{
 			UnregisterMessageHandler(typeof(TMessage));
 		}
 
-		public void UnregisterMessageHandler(Type t)
+		/// <summary>
+		/// Removes the message handler of the given type from the manager.
+		/// </summary>
+		/// <param name="type"></param>
+		public void UnregisterMessageHandler(Type type)
 		{
-			m_MessageHandlersSection.Execute(() => m_MessageHandlers.Remove(t));
+			m_MessageHandlersSection.Execute(() => m_MessageHandlers.Remove(type));
 		}
 
 		public HostInfo GetHostInfo()
 		{
-			string address = IcdEnvironment.NetworkAddresses.FirstOrDefault();
-			ushort port = NetworkUtils.GetDirectMessagePortForSystem(m_SystemId);
-
-			return new HostInfo(address, port);
+			return NetworkUtils.GetLocalHostInfo(m_SystemId);
 		}
 
 		public void Respond(uint clientId, Guid originalMessageId, AbstractMessage response)
 		{
-			if (clientId > 0 && response != null && m_Server.ClientConnected(clientId))
-			{
-				response.MessageId = originalMessageId;
-				response.MessageFrom = GetHostInfo();
-				m_Server.Send(clientId, response.Serialize());
-			}
+			if (clientId <= 0 || response == null || !m_Server.ClientConnected(clientId))
+				return;
+
+			response.MessageId = originalMessageId;
+			response.MessageFrom = GetHostInfo();
+			m_Server.Send(clientId, response.Serialize());
 		}
 
 		#endregion
@@ -141,6 +152,7 @@ namespace ICD.Connect.Protocol.Network.Direct
 			message.MessageId = Guid.NewGuid();
 			message.MessageFrom = GetHostInfo();
 			string data = message.Serialize();
+
 			AsyncTcpClient client = m_ClientPool.GetClient(sendTo);
 			client.Send(data);
 		}
@@ -156,14 +168,93 @@ namespace ICD.Connect.Protocol.Network.Direct
 			where TResponse : AbstractMessage
 		{
 			Guid messageId = Guid.NewGuid();
+
 			message.MessageId = messageId;
 			message.MessageFrom = GetHostInfo();
 			string data = message.Serialize();
+
 			AsyncTcpClient client = m_ClientPool.GetClient(sendTo);
 			m_MessageCallbacks.Add(messageId, response => callback(response as TResponse));
 			client.Send(data);
 		}
 
+		#endregion
+
+		#region Server Buffer Callbacks
+
+		/// <summary>
+		/// Subscribe to the server buffer manager events.
+		/// </summary>
+		/// <param name="manager"></param>
+		private void Subscribe(TcpServerBufferManager manager)
+		{
+			manager.OnClientCompletedSerial += ServerBufferOnClientCompletedSerial;
+		}
+
+		/// <summary>
+		/// Unsubscribe from the server buffer manager events.
+		/// </summary>
+		/// <param name="manager"></param>
+		private void Unsubscribe(TcpServerBufferManager manager)
+		{
+			manager.OnClientCompletedSerial -= ServerBufferOnClientCompletedSerial;
+		}
+
+		/// <summary>
+		/// Called when a buffer completes a serial response.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="clientId"></param>
+		/// <param name="data"></param>
+		private void ServerBufferOnClientCompletedSerial(TcpServerBufferManager sender, uint clientId, string data)
+		{
+			AbstractMessage msg = AbstractMessage.Deserialize(data);
+			if (msg == null)
+				return;
+
+			msg.ClientId = clientId;
+
+			Type type = msg.GetType();
+			if (!m_MessageHandlers.ContainsKey(type))
+				return;
+
+			AbstractMessage response = m_MessageHandlers[type].HandleMessage(msg);
+			if (response == null || !m_Server.ClientConnected(clientId))
+				return;
+
+			response.MessageId = msg.MessageId;
+			response.MessageFrom = GetHostInfo();
+			m_Server.Send(clientId, response.Serialize());
+		}
+
+		#endregion
+
+		#region Client Buffer Callbacks
+
+		/// <summary>
+		/// Subscribe to the client buffer manager events.
+		/// </summary>
+		/// <param name="manager"></param>
+		private void Subscribe(TcpClientPoolBufferManager manager)
+		{
+			manager.OnClientCompletedSerial += ClientPoolBufferOnClientCompletedSerial;
+		}
+
+		/// <summary>
+		/// Unsubscribe from the client buffer manager events.
+		/// </summary>
+		/// <param name="manager"></param>
+		private void Unsubscribe(TcpClientPoolBufferManager manager)
+		{
+			manager.OnClientCompletedSerial -= ClientPoolBufferOnClientCompletedSerial;
+		}
+
+		/// <summary>
+		/// Called when a buffer completes a serial response.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="client"></param>
+		/// <param name="data"></param>
 		private void ClientPoolBufferOnClientCompletedSerial(TcpClientPoolBufferManager sender, AsyncTcpClient client,
 		                                                     string data)
 		{
@@ -181,17 +272,5 @@ namespace ICD.Connect.Protocol.Network.Direct
 		}
 
 		#endregion
-
-		public void Dispose()
-		{
-			m_MessageCallbacks.Clear();
-
-			foreach (IDisposable handler in m_MessageHandlers.Values.OfType<IDisposable>())
-				handler.Dispose();
-
-			m_MessageHandlers.Clear();
-			m_Server.Dispose();
-			m_ClientPool.Dispose();
-		}
 	}
 }
