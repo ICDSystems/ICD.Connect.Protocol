ab353d72f4d1028e825c544e8d67f8ecf5d4c90b
Chris Cameron <chris.cameron@wedoresi.com>
2017-09-07 16:57:38 -0400

    Attempt at fixing log spam on TCPServer.DisconnectAll()

--- ICD.Connect.Protocol.Network/Tcp/AsyncTcpServer.SimplSharp.cs
+++ ICD.Connect.Protocol.Network/Tcp/AsyncTcpServer.SimplSharp.cs
@@ -43,8 +43,9 @@ namespace ICD.Connect.Protocol.Network.Tcp
 
 			if (m_TcpListener != null)
 			{
-				m_TcpListener.DisconnectAll();
 				m_TcpListener.SocketStatusChange -= HandleSocketStatusChange;
+				m_TcpListener.DisconnectAll();
+				
 				ServiceProvider.TryGetService<ILoggerService>()
 				               .AddEntry(eSeverity.Notice,
 				                         string.Format("AsyncTcpServer No Longer Listening on Port {0}", m_TcpListener.PortNumber));
