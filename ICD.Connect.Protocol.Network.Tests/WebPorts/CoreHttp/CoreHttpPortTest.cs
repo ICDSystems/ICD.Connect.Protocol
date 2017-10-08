e23ddd30f2ca12f9e08b9790aa910af142e5c4a5
Chris Cameron <chris.cameron@wedoresi.com>
2017-10-08 18:07:40 -0400

    Adding Get and Post tests for web port

--- ICD.Connect.Protocol.Network.Tests/WebPorts/CoreHttp/CoreHttpPortTest.cs
+++ ICD.Connect.Protocol.Network.Tests/WebPorts/CoreHttp/CoreHttpPortTest.cs
@@ -1,4 +1,7 @@
-﻿using ICD.Connect.Protocol.Network.WebPorts.CoreHttp;
+﻿using System.Text;
+using ICD.Common.Properties;
+using ICD.Connect.Protocol.Network.WebPorts.CoreHttp;
+using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace ICD.Connect.Protocol.Network.Tests.WebPorts.CoreHttp
@@ -6,6 +9,22 @@ namespace ICD.Connect.Protocol.Network.Tests.WebPorts.CoreHttp
 	[TestFixture]
     public sealed class CoreHttpPortTest
     {
+		// ReSharper disable once ClassNeverInstantiated.Local
+		private sealed class Post
+		{
+			[UsedImplicitly]
+			public int UserId { get; set; }
+
+			[UsedImplicitly]
+			public int Id { get; set; }
+
+			[UsedImplicitly]
+			public string Title { get; set; }
+
+			[UsedImplicitly]
+			public string Body { get; set; }
+		}
+
 		[TestCase("http://127.0.0.1/")]
 		[TestCase("http://google.com/")]
 		public void AddressTest(string address)
@@ -69,13 +88,46 @@ namespace ICD.Connect.Protocol.Network.Tests.WebPorts.CoreHttp
 		[Test]
 		public void GetTest()
 		{
-			Assert.Inconclusive();
+			const string address = "http://jsonplaceholder.typicode.com/";
+			const string request = "posts/1";
+
+			CoreHttpPort port = new CoreHttpPort
+			{
+				Address = address,
+				Accept = "application/json"
+			};
+
+			string result = port.Get(request);
+			Post post = JsonConvert.DeserializeObject<Post>(result);
+
+			Assert.AreEqual(1, post.Id);
+			Assert.AreEqual(1, post.UserId);
+			Assert.AreEqual("sunt aut facere repellat provident occaecati excepturi optio reprehenderit", post.Title);
+			Assert.AreEqual("quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto", post.Body);
 		}
 
 		[Test]
 		public void PostTest()
 		{
-			Assert.Inconclusive();
+			const string address = "http://jsonplaceholder.typicode.com/";
+			const string request = "posts";
+
+			CoreHttpPort port = new CoreHttpPort
+			{
+				Address = address,
+				Accept = "application/json"
+			};
+
+			const string dataString = @"{title: 'foo', body: 'bar', userId: 1}";
+			byte[] data = Encoding.ASCII.GetBytes(dataString);
+
+			string result = port.Post(request, data);
+			Post post = JsonConvert.DeserializeObject<Post>(result);
+
+			Assert.AreEqual(101, post.Id);
+			Assert.AreEqual(1, post.UserId);
+			Assert.AreEqual("foo", post.Title);
+			Assert.AreEqual("bar", post.Body);
 		}
 
 		[Test]
