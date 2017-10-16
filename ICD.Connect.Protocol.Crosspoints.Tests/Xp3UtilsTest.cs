8a71de5c482e5e62d27d50f675e929b41e1b713c
Chris Cameron <chris.cameron@wedoresi.com>
2017-10-16 15:35:06 -0400

    Initial commit of Xp3UtilsTest

---
+++ ICD.Connect.Protocol.Crosspoints.Tests/Xp3UtilsTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace ICD.Connect.Protocol.Crosspoints.Tests
+{
+	[TestFixture]
+	public sealed class Xp3UtilsTest
+	{
+		[Test]
+		public void GetAdvertisementMulticastPortsTest()
+		{
+			ushort[] ports = Xp3Utils.GetAdvertisementMulticastPorts(1).ToArray();
+			Assert.AreEqual(10, ports.Length);
+			Assert.AreEqual(10, ports.Distinct().Count());
+		}
+
+		[TestCase((uint)1, 1, (ushort)30010)]
+		[TestCase((uint)1, 2, (ushort)30020)]
+		[TestCase((uint)2, 1, (ushort)30011)]
+		public void GetPortForSlotAndSystemTest(uint programNumber, int systemId, ushort expected)
+		{
+			Assert.AreEqual(expected, Xp3Utils.GetPortForSlotAndSystem(programNumber, systemId));
+		}
+
+		[TestCase(1, (ushort)30010)]
+		public void GetPortForSystemTest(int systemId, ushort expected)
+		{
+			Assert.AreEqual(expected, Xp3Utils.GetPortForSystem(systemId));
+		}
+	}
+}
