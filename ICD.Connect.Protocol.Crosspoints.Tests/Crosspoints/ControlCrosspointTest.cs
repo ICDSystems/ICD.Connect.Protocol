56b2e9d2a733471b75728d7545268b52185dee63
Chris Cameron <chris.cameron@wedoresi.com>
2017-10-17 11:10:15 -0400

    Resurrecting ControlCrosspointTest and SigCacheTest

---
+++ ICD.Connect.Protocol.Crosspoints.Tests/Crosspoints/ControlCrosspointTest.cs
@@ -0,0 +1,126 @@
+﻿using ICD.Common.Properties;
+using ICD.Connect.Protocol.Crosspoints.Crosspoints;
+using ICD.SimplSharp.XP3.Tests.Crosspoints;
+using NUnit.Framework;
+
+namespace ICD.Connect.Protocol.Crosspoints.Tests.Crosspoints
+{
+	[TestFixture, UsedImplicitly]
+	public sealed class ControlCrosspointTest : AbstractCrosspointTest
+	{
+		protected override AbstractCrosspoint InstantiateCrosspoint(int id, string name)
+		{
+			return new ControlCrosspoint(id, name);
+		}
+
+		[UsedImplicitly]
+		[TestCase(100)]
+		public void InitializeTest(int equipmentId)
+		{
+			ControlCrosspoint crosspoint = InstantiateCrosspoint(1, null) as ControlCrosspoint;
+
+			// No connect callback
+			Assert.IsFalse(crosspoint.Initialize(equipmentId));
+
+			// Successful initialize
+			int[] requestConnectCount = {0};
+			int requestConnectEquipmentId = -1;
+			int[] requestDisconnectCount = {0};
+
+			crosspoint.RequestConnectCallback = (sender, id) =>
+			                                    {
+				                                    requestConnectCount[0]++;
+				                                    requestConnectEquipmentId = id;
+				                                    return eCrosspointStatus.Connected;
+			                                    };
+
+			crosspoint.RequestDisconnectCallback = sender =>
+			                                       {
+				                                       requestDisconnectCount[0]++;
+				                                       return eCrosspointStatus.Idle;
+			                                       };
+
+			Assert.IsTrue(crosspoint.Initialize(equipmentId));
+			Assert.AreEqual(0, requestDisconnectCount[0]);
+			Assert.AreEqual(1, requestConnectCount[0]);
+			Assert.AreEqual(equipmentId, requestConnectEquipmentId);
+			Assert.AreEqual(equipmentId, crosspoint.EquipmentCrosspoint);
+
+			// Initialize the same equipment again
+			requestConnectCount[0] = 0;
+			requestDisconnectCount[0] = 0;
+
+			Assert.IsFalse(crosspoint.Initialize(equipmentId));
+			Assert.AreEqual(0, requestDisconnectCount[0]);
+			Assert.AreEqual(0, requestConnectCount[0]);
+			Assert.AreEqual(equipmentId, crosspoint.EquipmentCrosspoint);
+
+			// Deinit
+			requestConnectCount[0] = 0;
+			requestConnectEquipmentId = -1;
+			requestDisconnectCount[0] = 0;
+
+			Assert.IsTrue(crosspoint.Initialize(Xp3Utils.NULL_EQUIPMENT));
+			Assert.AreEqual(1, requestDisconnectCount[0]);
+			Assert.AreEqual(0, requestConnectCount[0]);
+			Assert.AreEqual(Xp3Utils.NULL_EQUIPMENT, crosspoint.EquipmentCrosspoint);
+
+			// Fail init
+			requestConnectCount[0] = 0;
+			requestConnectEquipmentId = -1;
+			requestDisconnectCount[0] = 0;
+
+			crosspoint.RequestConnectCallback = (sender, id) =>
+			                                    {
+				                                    requestConnectCount[0]++;
+				                                    requestConnectEquipmentId = id;
+				                                    return eCrosspointStatus.ConnectFailed;
+			                                    };
+
+			Assert.IsFalse(crosspoint.Initialize(equipmentId));
+			Assert.AreEqual(0, requestDisconnectCount[0]);
+			Assert.AreEqual(1, requestConnectCount[0]);
+			Assert.AreEqual(equipmentId, requestConnectEquipmentId);
+			Assert.AreEqual(Xp3Utils.NULL_EQUIPMENT, crosspoint.EquipmentCrosspoint);
+
+			/*
+			// Fail deinit
+			requestConnectCount = 0;
+			requestConnectEquipmentId = -1;
+			requestDisconnectCount = 0;
+
+			crosspoint.RequestConnectCallback = (sender, id) =>
+			{
+				requestConnectCount++;
+				requestConnectEquipmentId = id;
+				return true;
+			};
+
+			crosspoint.RequestDisconnectCallback = sender =>
+			{
+				requestDisconnectCount++;
+				return true;
+			};
+
+			Assert.IsTrue(crosspoint.Initialize(equipmentId));
+			Assert.AreEqual(1, requestDisconnectCount);
+			Assert.AreEqual(1, requestConnectCount);
+			Assert.AreEqual(equipmentId, requestConnectEquipmentId);
+			Assert.AreEqual(equipmentId, crosspoint.EquipmentCrosspoint);
+			 */
+
+			Assert.Inconclusive();
+		}
+
+		[Test, UsedImplicitly]
+		public void DeinitializeTest()
+		{
+			/*
+			EquipmentCrosspoint = Xp3Utils.NULL_EQUIPMENT;
+
+			return RequestDisconnectCallback != null && RequestDisconnectCallback(this);
+			 */
+			Assert.Inconclusive();
+		}
+	}
+}
---
+++ ICD.Connect.Protocol.Tests/Sigs/SigCacheTest.cs
@@ -0,0 +1,100 @@
+﻿using System.Linq;
+using ICD.Common.Properties;
+using ICD.Connect.Protocol.Sigs;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace ICD.Connect.Protocol.Tests.Sigs
+{
+	[TestFixture, UsedImplicitly]
+	public sealed class SigCacheTest
+	{
+		[Test, UsedImplicitly]
+		public void ClearTest()
+		{
+			SigCache cache = new SigCache
+			{
+				new SigInfo(1, null, 0, false),
+				new SigInfo(1, null, 1, false),
+				new SigInfo(1, null, 1, false),
+
+				new SigInfo(0, "test", 0, false),
+				new SigInfo(0, "test", 1, false),
+				new SigInfo(0, "test", 1, false)
+			};
+
+			Assert.AreEqual(4, cache.Count);
+
+			cache.Clear();
+
+			Assert.AreEqual(0, cache.Count);
+		}
+
+		[Test, UsedImplicitly]
+		public void AddsTest()
+		{
+			SigCache cache = new SigCache();
+
+			cache.AddRange(new[]
+			{
+				new SigInfo(1, null, 0, false),
+				new SigInfo(1, null, 0, false),
+				new SigInfo(0, "test", 0, false),
+				new SigInfo(0, "test", 0, false)
+			});
+
+			Assert.AreEqual(2, cache.Count);
+		}
+
+		[Test, UsedImplicitly]
+		public void AddTest()
+		{
+			// ReSharper disable once UseObjectOrCollectionInitializer
+			SigCache cache = new SigCache();
+
+			cache.Add(new SigInfo(1, null, 0, false));
+			cache.Add(new SigInfo(1, null, 0, false));
+			cache.Add(new SigInfo(0, "test", 0, false));
+			cache.Add(new SigInfo(0, "test", 0, false));
+
+			Assert.AreEqual(2, cache.Count);
+		}
+
+		[Test, UsedImplicitly]
+		public void RemoveSigTest()
+		{
+			SigCache cache = new SigCache();
+
+			SigInfo a = new SigInfo(1, null, 0, false);
+			SigInfo b = new SigInfo(1, null, 1, false);
+
+			cache.Add(a);
+			cache.Add(b);
+
+			Assert.AreEqual(2, cache.Count);
+
+			cache.Remove(a);
+
+			Assert.AreEqual(1, cache.Count);
+		}
+
+		[Test, UsedImplicitly]
+		public void GetSigsTest()
+		{
+			SigCache cache = new SigCache();
+
+			SigInfo numbered = new SigInfo(1, null, 0, false);
+			SigInfo named = new SigInfo(0, "test", 0, false);
+
+			cache.Add(numbered);
+			cache.Add(named);
+
+			SigInfo[] sigs = cache.ToArray();
+
+			Assert.AreEqual(2, sigs.Length);
+
+			Assert.IsTrue(sigs.Any(s => s == numbered));
+			Assert.IsTrue(sigs.Any(s => s == named));
+		}
+	}
+}
