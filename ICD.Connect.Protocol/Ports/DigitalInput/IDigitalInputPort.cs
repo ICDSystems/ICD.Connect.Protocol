bb6115a99c3e729505ee55a6d910f34237a67d18
Jack Kanarish <jack.kanarish@wedoresi.com>
2017-10-31 15:08:22 -0400

    Create Digital Input

---
+++ ICD.Connect.Protocol/Ports/DigitalInput/IDigitalInputPort.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ICD.Common.Utils.EventArguments;
+
+namespace ICD.Connect.Protocol.Ports.DigitalInput
+{
+
+	public interface IDigitalInputPort : IPort
+	{
+		/// <summary>
+		/// Raised when a change in the digital input signal is detected.
+		/// </summary>
+		event EventHandler<BoolEventArgs> OnStateChanged;
+
+		/// <summary>
+		/// Gets the current digital input state.
+		/// </summary>
+		bool State { get; }
+	}
+}
