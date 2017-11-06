0b3207ae0260cf602f79793738aa2ffdb66d154c
Chris Cameron <chris.cameron@wedoresi.com>
2017-11-06 18:10:10 -0500

    Initial commit of XSigEventArgs

---
+++ ICD.Connect.Protocol/EventArguments/XSigEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using ICD.Common.Utils.EventArguments;
+using ICD.Connect.Protocol.XSig;
+
+namespace ICD.Connect.Protocol.EventArguments
+{
+	public sealed class XSigEventArgs : GenericEventArgs<IXSig>
+	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="data"></param>
+		public XSigEventArgs(IXSig data)
+			: base(data)
+		{
+		}
+	}
+}
