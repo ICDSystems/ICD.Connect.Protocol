8cd65514db943592e9a3ce2efbd9cb2f47f2dcd3
Chris Cameron <chris.cameron@wedoresi.com>
2017-11-30 13:20:21 -0500

    Adding net standard projects

--- ICD.Connect.Protocol.Mock/Properties/AssemblyInfo.cs
+++ ICD.Connect.Protocol.Mock/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("ICD.Connect.Protocol.Mock")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Protocol.Mock")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2017")]
 [assembly: AssemblyVersion("1.0.0.*")]
 
