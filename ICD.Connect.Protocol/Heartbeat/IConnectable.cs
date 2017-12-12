10e7c2c9e296f909c8c4a723a0e62cfcffcc941c
Jack Kanarish <jack.kanarish@wedoresi.com>
2017-12-12 16:13:52 -0500

    Heartbeat commit

---
+++ ICD.Connect.Protocol/Heartbeat/IConnectable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ICD.Common.Utils.EventArguments;
+
+namespace ICD.Connect.Protocol.Heartbeat
+{
+    public interface IConnectable
+    {
+        event EventHandler<BoolEventArgs> OnConnectedStateChanged;
+        bool IsConnected { get; }
+        Heartbeat Heartbeat { get; }
+        void Connect();
+        void Disconnect();
+    }
+
+}
