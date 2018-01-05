baa81801cdf05117abb0c3f6ea901db38536f91c
Chris Cameron <chris.cameron@wedoresi.com>
2018-01-05 11:50:47 -0500

    Tidying

--- ICD.Connect.Protocol/Heartbeat/IConnectable.cs
+++ ICD.Connect.Protocol/Heartbeat/IConnectable.cs
@@ -3,13 +3,31 @@ using ICD.Common.Utils.EventArguments;
 
 namespace ICD.Connect.Protocol.Heartbeat
 {
-    public interface IConnectable
-    {
-        event EventHandler<BoolEventArgs> OnConnectedStateChanged;
-        bool IsConnected { get; }
-        Heartbeat Heartbeat { get; }
-        void Connect();
-        void Disconnect();
-    }
+	public interface IConnectable
+	{
+		/// <summary>
+		/// Raised when the connected state changes.
+		/// </summary>
+		event EventHandler<BoolEventArgs> OnConnectedStateChanged;
 
+		/// <summary>
+		/// Gets the current connected state of the instance.
+		/// </summary>
+		bool IsConnected { get; }
+
+		/// <summary>
+		/// Gets the heartbeat instance that is enforcing the connection state.
+		/// </summary>
+		Heartbeat Heartbeat { get; }
+
+		/// <summary>
+		/// Connect the instance to the remote endpoint.
+		/// </summary>
+		void Connect();
+
+		/// <summary>
+		/// Disconnects the instance from the remote endpoint.
+		/// </summary>
+		void Disconnect();
+	}
 }
