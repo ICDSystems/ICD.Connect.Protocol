eaf29bf60ba7da71fa3aa265dc6f03b8d15ba2a0
Chris Cameron <chris.cameron@wedoresi.com>
2018-01-15 21:02:54 -0500

    Fixing using directives

--- ICD.Connect.Protocol.Crosspoints/Crosspoints/ControlCrosspoint.cs
+++ ICD.Connect.Protocol.Crosspoints/Crosspoints/ControlCrosspoint.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using ICD.Common.Properties;
-using ICD.Common.Services.Logging;
 using ICD.Common.Utils;
+using ICD.Common.Utils.Services.Logging;
 using ICD.Connect.API.Commands;
 using ICD.Connect.API.Nodes;
 using ICD.Connect.Protocol.Sigs;
--- ICD.Connect.Protocol.Crosspoints/Crosspoints/EquipmentCrosspoint.cs
+++ ICD.Connect.Protocol.Crosspoints/Crosspoints/EquipmentCrosspoint.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using ICD.Common.Services.Logging;
 using ICD.Common.Utils;
 using ICD.Common.Utils.Collections;
 using ICD.Common.Utils.EventArguments;
 using ICD.Common.Utils.Extensions;
+using ICD.Common.Utils.Services.Logging;
 using ICD.Connect.API.Nodes;
 using ICD.Connect.Protocol.Sigs;
 
