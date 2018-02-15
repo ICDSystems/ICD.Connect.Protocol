b7e5fa6b2d9c5383add30c4a2e6095bc704c67c8
Chris Cameron <chris.cameron@wedoresi.com>
2018-02-14 21:11:42 -0500

    Whitespace

--- ICD.Connect.Protocol.Network/Tcp/AsyncTcpClient.Standard.cs
+++ ICD.Connect.Protocol.Network/Tcp/AsyncTcpClient.Standard.cs
@@ -13,8 +13,8 @@ namespace ICD.Connect.Protocol.Network.Tcp
 	public sealed partial class AsyncTcpClient
 	{
 		private TcpClient m_TcpClient;
-        private NetworkStream m_Stream;
-        private readonly byte[] m_Buffer = new byte[DEFAULT_BUFFER_SIZE];
+		private NetworkStream m_Stream;
+		private readonly byte[] m_Buffer = new byte[DEFAULT_BUFFER_SIZE];
 
 		/// <summary>
 		/// Connects to the remote end point Asyncrohnously
@@ -41,8 +41,8 @@ namespace ICD.Connect.Protocol.Network.Tcp
 					return;
 				}
 
-                m_Stream = m_TcpClient.GetStream();
-                m_Stream.ReadAsync(m_Buffer, 0, m_Buffer.Length).ContinueWith(TcpClientReceiveHandler);
+				m_Stream = m_TcpClient.GetStream();
+				m_Stream.ReadAsync(m_Buffer, 0, m_Buffer.Length).ContinueWith(TcpClientReceiveHandler);
 			}
 			catch (Exception e)
 			{
@@ -54,13 +54,13 @@ namespace ICD.Connect.Protocol.Network.Tcp
 
 				UpdateIsConnectedState();
 			}
-        }
+		}
 
-        /// <summary>
-        /// Gets the current connection state of the wrapped TCP client.
-        /// </summary>
-        /// <returns></returns>
-        protected override bool GetIsConnectedState()
+		/// <summary>
+		/// Gets the current connection state of the wrapped TCP client.
+		/// </summary>
+		/// <returns></returns>
+		protected override bool GetIsConnectedState()
 		{
 			return m_TcpClient != null && m_TcpClient.Client.Connected;
 		}
@@ -82,24 +82,24 @@ namespace ICD.Connect.Protocol.Network.Tcp
 		}
 
 		/// <summary>
-        /// Sends a Byte for Byte string (ISO-8859-1)
-        /// </summary>
-        /// <param name="data"></param>
-        /// <returns></returns>
-        protected override bool SendFinal(string data)
-        {
-            byte[] bytes = StringUtils.ToBytes(data);
-            try
-            {
-                m_TcpClient.Client.Send(bytes, 0, bytes.Length, SocketFlags.None);
-                PrintTx(data);
-                return true;
-            }
-            finally
-            {
-                UpdateIsConnectedState();
-            }
-        }
+		/// Sends a Byte for Byte string (ISO-8859-1)
+		/// </summary>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		protected override bool SendFinal(string data)
+		{
+			byte[] bytes = StringUtils.ToBytes(data);
+			try
+			{
+				m_TcpClient.Client.Send(bytes, 0, bytes.Length, SocketFlags.None);
+				PrintTx(data);
+				return true;
+			}
+			finally
+			{
+				UpdateIsConnectedState();
+			}
+		}
 
 		/// <summary>
 		/// Handles Receiving Data from the Active TCP Connection
@@ -110,7 +110,8 @@ namespace ICD.Connect.Protocol.Network.Tcp
 			if (task.IsFaulted)
 			{
 				string message = task.Exception.InnerExceptions.First().Message;
-				Logger.AddEntry(eSeverity.Error, "{0} failed to receive data from host {1}:{2} - {3}", this, Address, Port, message);
+				Logger.AddEntry(eSeverity.Error, "{0} failed to receive data from host {1}:{2} - {3}", this, Address, Port,
+								message);
 				UpdateIsConnectedState();
 				return;
 			}
@@ -131,17 +132,17 @@ namespace ICD.Connect.Protocol.Network.Tcp
 		}
 
 		/// <summary>
-        /// Calls the delegate for each console status item.
-        /// </summary>
-        /// <param name="addRow"></param>
-        public override void BuildConsoleStatus(AddStatusRowDelegate addRow)
-        {
-            base.BuildConsoleStatus(addRow);
+		/// Calls the delegate for each console status item.
+		/// </summary>
+		/// <param name="addRow"></param>
+		public override void BuildConsoleStatus(AddStatusRowDelegate addRow)
+		{
+			base.BuildConsoleStatus(addRow);
 
-            addRow("Address", Address);
-            addRow("Port", Port);
-            addRow("Buffer Size", BufferSize);
-        }
-    }
+			addRow("Address", Address);
+			addRow("Port", Port);
+			addRow("Buffer Size", BufferSize);
+		}
+	}
 }
 #endif
