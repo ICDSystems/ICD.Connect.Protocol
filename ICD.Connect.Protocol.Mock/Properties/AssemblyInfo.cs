6384377eea536f12df4488642491478a8bf91148
Jack Kanarish <jack.kanarish@wedoresi.com>
2018-04-02 15:00:37 -0400

    new versioning commit

--- ICD.Connect.Protocol.Mock/Properties/AssemblyInfo.cs
+++ ICD.Connect.Protocol.Mock/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 [assembly: AssemblyTitle("ICD.Connect.Protocol.Mock")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Protocol.Mock")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
 
