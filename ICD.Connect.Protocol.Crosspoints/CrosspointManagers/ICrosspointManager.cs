4df0d46bfb9db83aa2a7cea03a8ef7f4f25d3046
Jack Kanarish <jack.kanarish@wedoresi.com>
2018-05-15 16:46:26 -0400

    fix: revert accidental comment deletion

--- ICD.Connect.Protocol.Crosspoints/CrosspointManagers/ICrosspointManager.cs
+++ ICD.Connect.Protocol.Crosspoints/CrosspointManagers/ICrosspointManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using ICD.Common.Properties;
 using ICD.Connect.API.Nodes;
 using ICD.Connect.Protocol.Crosspoints.Crosspoints;
 using ICD.Connect.Protocol.Ports;
@@ -10,12 +11,27 @@ namespace ICD.Connect.Protocol.Crosspoints.CrosspointManagers
 
 	public interface ICrosspointManager : IConsoleNode, IDisposable
 	{
+		/// <summary>
+		/// Raised when a crosspoint is registered with the manager.
+		/// </summary>
+		[PublicAPI]
 		event CrosspointManagerCrosspointCallback OnCrosspointRegistered;
+
+		/// <summary>
+		/// Raised when a crosspoint is unregistered from the manager.
+		/// </summary>
+		[PublicAPI]
 		event CrosspointManagerCrosspointCallback OnCrosspointUnregistered;
+
+		/// <summary>
+		/// Gets the address of the crosspoint manager.
+		/// </summary>
+		/// <returns></returns>
+		[PublicAPI]
+		HostInfo GetHostInfo();
 
 		int SystemId { get; }
 
-		HostInfo GetHostInfo();
 		IEnumerable<ICrosspoint> GetCrosspoints();
 
 		void RegisterCrosspoint(ICrosspoint crosspoint);
