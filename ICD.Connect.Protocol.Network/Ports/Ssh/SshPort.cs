cd5689d05c5b33f3b92ff8ae7ea457fd77e1cfe9
Chris Cameron <chris.cameron@wedoresi.com>
2018-10-17 16:05:16 -0400

    feat: SshPort implements private key authentication method

--- ICD.Connect.Protocol.Network/Ports/Ssh/SshPort.cs
+++ ICD.Connect.Protocol.Network/Ports/Ssh/SshPort.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using ICD.Common.Properties;
 using ICD.Common.Utils;
+using ICD.Common.Utils.Extensions;
 using ICD.Common.Utils.Services.Logging;
 using ICD.Connect.Protocol.Network.Settings;
 using ICD.Connect.Protocol.Ports;
@@ -26,10 +29,11 @@ namespace ICD.Connect.Protocol.Network.Ports.Ssh
 		public const ushort DEFAULT_PORT = 22;
 
 		private readonly SafeCriticalSection m_SshSection;
+		private readonly Dictionary<string, string> m_PrivateKeys;
+		private readonly List<PrivateKeyFile> m_PrivateKeyFiles;
+        private readonly SecureNetworkProperties m_NetworkProperties;
 
-		private readonly SecureNetworkProperties m_NetworkProperties;
-
-		private KeyboardInteractiveAuthenticationMethod m_Auth;
+		private KeyboardInteractiveAuthenticationMethod m_KeyboardAuth;
 		private ConnectionInfo m_ConnectionInfo;
 		private ShellStream m_SshStream;
 		private SshClient m_SshClient;
@@ -89,6 +93,8 @@ namespace ICD.Connect.Protocol.Network.Ports.Ssh
 		{
 			m_SshSection = new SafeCriticalSection();
 			m_NetworkProperties = new SecureNetworkProperties();
+			m_PrivateKeys = new Dictionary<string, string>();
+			m_PrivateKeyFiles = new List<PrivateKeyFile>();
 
 			Port = DEFAULT_PORT;
 		}
@@ -103,139 +109,6 @@ namespace ICD.Connect.Protocol.Network.Ports.Ssh
 			Disconnect();
 		}
 
-		#endregion
-
-		#region Methods
-
-		/// <summary>
-		/// Connect via SSH
-		/// </summary>
-		public override void Connect()
-		{
-			m_SshSection.Enter();
-
-			try
-			{
-				Disconnect();
-
-				if (Address == null)
-				{
-					Log(eSeverity.Error, "Failed to connect - Address is null");
-					return;
-				}
-
-				if (Username == null)
-				{
-					Log(eSeverity.Error, "Failed to connect - Username is null");
-					return;
-				}
-
-				m_Auth = new KeyboardInteractiveAuthenticationMethod(Username);
-				Subscribe(m_Auth);
-
-				AuthenticationMethod[] authMethods =
-				{
-					m_Auth,
-					new NoneAuthenticationMethod(Username),
-					new PasswordAuthenticationMethod(Username, Password)
-				};
-
-				m_ConnectionInfo = new ConnectionInfo(Address, Port, Username, authMethods);
-
-				try
-				{
-					m_SshClient = new SshClient(m_ConnectionInfo)
-					{
-						// Added keepalive based on Biamp SSH implementation
-						KeepAliveInterval = new TimeSpan(0, 0, 30)
-					};
-
-					// Added Subscribe here, mainly to catch HostKeyReceived events, to help initial connections
-					Subscribe(m_SshClient);
-					m_SshClient.Connect();
-				}
-				// Catches when we attempt to connect to an invalid/offline endpoint.
-				catch (SshException e)
-				{
-					// Potential fix for "Message type 80 is not valid" - the Crestron SSH implementation should be ignoring this.
-					if (!e.Message.Contains("Message type 80 is not valid"))
-					{
-						DisposeClient();
-						Log(eSeverity.Error, "Failed to connect - {0}", e.GetBaseException().Message);
-					}
-				}
-				// Catches when we attempt to connect to an invalid/offline endpoint.
-				catch (SocketException e)
-				{
-					DisposeClient();
-					Log(eSeverity.Error, "Failed to connect - {0}", e.GetBaseException().Message);
-				}
-
-				// ShellStream can only be instantiated when the client is connected.
-				if (m_SshClient == null || !m_SshClient.IsConnected)
-					return;
-
-				int failCount = 0;
-
-				while (failCount < 5)
-				{
-					try
-					{
-						m_SshStream = m_SshClient.CreateShellStream("Crestron SSH Session", 80, 120, 800, 600, 16384);
-						Subscribe(m_SshStream);
-					}
-					catch (SshException e)
-					{
-						failCount++;
-						DisposeStream();
-
-						// Potential fix for "Message type 80 is not valid" - the Crestron SSH implementation should be ignoring this.
-						if (e.Message.Contains("Message type 80 is not valid"))
-							continue;
-
-						Log(eSeverity.Error, "Failed to create shell stream - {0}", e.Message);
-					}
-
-					break;
-				}
-			}
-			finally
-			{
-				m_SshSection.Leave();
-
-				// Subscribe to the SSH Client outside of critical sections. We only subscribe to events to
-				// determine if the port loses connection, in which case a deadlock could happen.
-				// DREWS NOTE:  I moved this up into the critical section, because I think we need to respond to HostKeyReceived events.
-				//              (Based on looking at Biamp Module SSH implementation)
-				//              I'm not sure how this would cause a deadlock?
-				// Subscribe(m_SshClient);
-
-				UpdateIsConnectedState();
-			}
-		}
-
-		/// <summary>
-		/// SSH Disconnect
-		/// </summary>
-		public override void Disconnect()
-		{
-			m_SshSection.Enter();
-
-			try
-			{
-				DisposeAuth();
-				DisposeConnectionInfo();
-				DisposeStream();
-				DisposeClient();
-			}
-			finally
-			{
-				m_SshSection.Leave();
-
-				UpdateIsConnectedState();
-			}
-		}
-
 		/// <summary>
 		/// Dispose the auth object.
 		/// </summary>
@@ -245,13 +118,13 @@ namespace ICD.Connect.Protocol.Network.Ports.Ssh
 
 			try
 			{
-				if (m_Auth == null)
+				if (m_KeyboardAuth == null)
 					return;
 
-				Unsubscribe(m_Auth);
+				Unsubscribe(m_KeyboardAuth);
 
-				m_Auth.Dispose();
-				m_Auth = null;
+				m_KeyboardAuth.Dispose();
+				m_KeyboardAuth = null;
 			}
 			finally
 			{
@@ -361,6 +234,140 @@ namespace ICD.Connect.Protocol.Network.Ports.Ssh
 			}
 		}
 
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Connect via SSH
+		/// </summary>
+		public override void Connect()
+		{
+			m_SshSection.Enter();
+
+			try
+			{
+				Disconnect();
+
+				if (Address == null)
+				{
+					Log(eSeverity.Error, "Failed to connect - Address is null");
+					return;
+				}
+
+				if (Username == null)
+				{
+					Log(eSeverity.Error, "Failed to connect - Username is null");
+					return;
+				}
+
+				m_KeyboardAuth = new KeyboardInteractiveAuthenticationMethod(Username);
+				Subscribe(m_KeyboardAuth);
+
+				AuthenticationMethod[] authMethods =
+				{
+					m_KeyboardAuth,
+					new NoneAuthenticationMethod(Username),
+					new PasswordAuthenticationMethod(Username, Password),
+					new PrivateKeyAuthenticationMethod(Username, m_PrivateKeyFiles.ToArray())
+				};
+
+				m_ConnectionInfo = new ConnectionInfo(Address, Port, Username, authMethods);
+
+				try
+				{
+					m_SshClient = new SshClient(m_ConnectionInfo)
+					{
+						// Added keepalive based on Biamp SSH implementation
+						KeepAliveInterval = new TimeSpan(0, 0, 30)
+					};
+
+					// Added Subscribe here, mainly to catch HostKeyReceived events, to help initial connections
+					Subscribe(m_SshClient);
+					m_SshClient.Connect();
+				}
+				// Catches when we attempt to connect to an invalid/offline endpoint.
+				catch (SshException e)
+				{
+					// Potential fix for "Message type 80 is not valid" - the Crestron SSH implementation should be ignoring this.
+					if (!e.Message.Contains("Message type 80 is not valid"))
+					{
+						DisposeClient();
+						Log(eSeverity.Error, "Failed to connect - {0}", e.GetBaseException().Message);
+					}
+				}
+				// Catches when we attempt to connect to an invalid/offline endpoint.
+				catch (SocketException e)
+				{
+					DisposeClient();
+					Log(eSeverity.Error, "Failed to connect - {0}", e.GetBaseException().Message);
+				}
+
+				// ShellStream can only be instantiated when the client is connected.
+				if (m_SshClient == null || !m_SshClient.IsConnected)
+					return;
+
+				int failCount = 0;
+
+				while (failCount < 5)
+				{
+					try
+					{
+						m_SshStream = m_SshClient.CreateShellStream("Crestron SSH Session", 80, 120, 800, 600, 16384);
+						Subscribe(m_SshStream);
+					}
+					catch (SshException e)
+					{
+						failCount++;
+						DisposeStream();
+
+						// Potential fix for "Message type 80 is not valid" - the Crestron SSH implementation should be ignoring this.
+						if (e.Message.Contains("Message type 80 is not valid"))
+							continue;
+
+						Log(eSeverity.Error, "Failed to create shell stream - {0}", e.Message);
+					}
+
+					break;
+				}
+			}
+			finally
+			{
+				m_SshSection.Leave();
+
+				// Subscribe to the SSH Client outside of critical sections. We only subscribe to events to
+				// determine if the port loses connection, in which case a deadlock could happen.
+				// DREWS NOTE:  I moved this up into the critical section, because I think we need to respond to HostKeyReceived events.
+				//              (Based on looking at Biamp Module SSH implementation)
+				//              I'm not sure how this would cause a deadlock?
+				// Subscribe(m_SshClient);
+
+				UpdateIsConnectedState();
+			}
+		}
+
+		/// <summary>
+		/// SSH Disconnect
+		/// </summary>
+		public override void Disconnect()
+		{
+			m_SshSection.Enter();
+
+			try
+			{
+				DisposeAuth();
+				DisposeConnectionInfo();
+				DisposeStream();
+				DisposeClient();
+			}
+			finally
+			{
+				m_SshSection.Leave();
+
+				UpdateIsConnectedState();
+			}
+		}
+
 		/// <summary>
 		/// Send command via SSH
 		/// </summary>
@@ -406,9 +413,124 @@ namespace ICD.Connect.Protocol.Network.Ports.Ssh
 			}
 		}
 
+		/// <summary>
+		/// Clears the private keys.
+		/// </summary>
+		public void ClearPrivateKeys()
+		{
+			SetPrivateKeys(Enumerable.Empty<KeyValuePair<string, string>>());
+		}
+
+		/// <summary>
+		/// Gets the private keys path -> pass-phrase mapping.
+		/// </summary>
+		/// <returns></returns>
+		public IEnumerable<KeyValuePair<string, string>> GetPrivateKeys()
+		{
+			return m_SshSection.Execute(() => m_PrivateKeys.ToArray(m_PrivateKeys.Count));
+		}
+
+		/// <summary>
+		/// Sets the private keys path -> pass-phrase mapping.
+		/// </summary>
+		/// <param name="privateKeys"></param>
+		public void SetPrivateKeys(IEnumerable<KeyValuePair<string, string>> privateKeys)
+		{
+			if (privateKeys == null)
+				throw new ArgumentNullException("privateKeys");
+
+			m_SshSection.Enter();
+
+			try
+			{
+				m_PrivateKeys.Clear();
+				m_PrivateKeys.AddRange(privateKeys);
+
+				IEnumerable<PrivateKeyFile> privateKeyFiles =
+					m_PrivateKeys.Select(kvp => ToPrivateKeyFile(kvp))
+					             .Where(f => f != null);
+
+				m_PrivateKeyFiles.Clear();
+				m_PrivateKeyFiles.AddRange(privateKeyFiles);
+			}
+			finally
+			{
+				m_SshSection.Leave();
+			}
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		/// <summary>
+		/// Gets the current connection state of the wrapped SSH client.
+		/// </summary>
+		/// <returns></returns>
+		protected override bool GetIsConnectedState()
+		{
+			return m_SshClient != null && m_SshClient.IsConnected && m_SshStream != null;
+		}
+
+		/// <summary>
+		/// Override to add additional properties to the ToString representation.
+		/// </summary>
+		/// <param name="addPropertyAndValue"></param>
+		protected override void BuildStringRepresentationProperties(Action<string, object> addPropertyAndValue)
+		{
+			base.BuildStringRepresentationProperties(addPropertyAndValue);
+
+			addPropertyAndValue("Host", new HostInfo(Address, Port));
+		}
+
+		/// <summary>
+		/// Converts the path -> pass-phrase kvp to a PrivateKeyFile.
+		/// </summary>
+		/// <param name="pair"></param>
+		/// <returns></returns>
+		[CanBeNull]
+		private PrivateKeyFile ToPrivateKeyFile(KeyValuePair<string, string> pair)
+		{
+			return ToPrivateKeyFile(pair.Key, pair.Value);
+		}
+
+		/// <summary>
+		/// Converts the path and pass-phrase to a PrivateKeyFile.
+		/// </summary>
+		/// <param name="path"></param>
+		/// <param name="passPhrase"></param>
+		/// <returns></returns>
+		[CanBeNull]
+		private PrivateKeyFile ToPrivateKeyFile(string path, string passPhrase)
+		{
+			path = PathUtils.GetProgramConfigPath("PrivateKeys", path);
+
+			try
+			{
+				return new PrivateKeyFile(path, passPhrase);
+			}
+			catch (Exception e)
+			{
+				Log(eSeverity.Error, "Failed to load PrivateKeyFile - {0}", e.Message);
+				return null;
+			}
+		}
+
 		#endregion
 
 		#region Settings
+
+		/// <summary>
+		/// Override to apply properties to the settings instance.
+		/// </summary>
+		/// <param name="settings"></param>
+		protected override void CopySettingsFinal(SshPortSettings settings)
+		{
+			base.CopySettingsFinal(settings);
+
+			IEnumerable<KeyValuePair<string, string>> privateKeys = GetPrivateKeys();
+			settings.SetPrivateKeys(privateKeys);
+		}
 
 		/// <summary>
 		/// Override to clear the instance settings.
@@ -416,6 +538,8 @@ namespace ICD.Connect.Protocol.Network.Ports.Ssh
 		protected override void ClearSettingsFinal()
 		{
 			base.ClearSettingsFinal();
+
+			ClearPrivateKeys();
 
 			ApplyConfiguration();
 		}
@@ -428,6 +552,9 @@ namespace ICD.Connect.Protocol.Network.Ports.Ssh
 		protected override void ApplySettingsFinal(SshPortSettings settings, IDeviceFactory factory)
 		{
 			base.ApplySettingsFinal(settings, factory);
+
+			IEnumerable<KeyValuePair<string, string>> privateKeys = settings.GetPrivateKeys();
+			SetPrivateKeys(privateKeys);
 
 			ApplyConfiguration();
 		}
@@ -510,6 +637,7 @@ namespace ICD.Connect.Protocol.Network.Ports.Ssh
 		private void SshStreamOnErrorOccurred(object sender, ExceptionEventArgs args)
 		{
 			Log(eSeverity.Error, args.Exception, "SSH Stream Error - {0}", args.Exception.Message);
+
 			UpdateIsConnectedState();
 		}
 
@@ -567,19 +695,6 @@ namespace ICD.Connect.Protocol.Network.Ports.Ssh
 		{
 			Log(eSeverity.Error, "Internal Error - {0}", args.Exception.Message);
 			UpdateIsConnectedState();
-		}
-
-		#endregion
-
-		#region Private Methods
-
-		/// <summary>
-		/// Gets the current connection state of the wrapped SSH client.
-		/// </summary>
-		/// <returns></returns>
-		protected override bool GetIsConnectedState()
-		{
-			return m_SshClient != null && m_SshClient.IsConnected && m_SshStream != null;
 		}
 
 		#endregion
