a44c5d28c39395d426f0122836d29d31403185f8
Chris Cameron <chris.cameron@wedoresi.com>
2018-11-29 14:12:03 -0500

    feat: Implementing com spec for mock com port

--- ICD.Connect.Protocol.Mock/Ports/ComPort/MockComPort.cs
+++ ICD.Connect.Protocol.Mock/Ports/ComPort/MockComPort.cs
@@ -13,10 +13,63 @@ namespace ICD.Connect.Protocol.Mock.Ports.ComPort
 
 		private readonly ComSpecProperties m_ComSpecProperties;
 
+		private eComBaudRates m_BaudRate;
+		private eComDataBits m_NumberOfDataBits;
+		private eComParityType m_ParityType;
+		private eComStopBits m_NumberOfStopBits;
+		private eComProtocolType m_ProtocolType;
+		private eComHardwareHandshakeType m_HardwareHandShake;
+		private eComSoftwareHandshakeType m_SoftwareHandshake;
+		private bool m_ReportCtsChanges;
+
+		#region Properties
+
 		/// <summary>
 		/// Gets the Com Spec configuration properties.
 		/// </summary>
-		protected override IComSpecProperties ComSpecProperties { get { return m_ComSpecProperties; } }
+		public override IComSpecProperties ComSpecProperties { get { return m_ComSpecProperties; } }
+
+		/// <summary>
+		/// Gets the baud rate.
+		/// </summary>
+		public override eComBaudRates BaudRate { get { return m_BaudRate; } }
+
+		/// <summary>
+		/// Gets the number of data bits.
+		/// </summary>
+		public override eComDataBits NumberOfDataBits { get { return m_NumberOfDataBits; } }
+
+		/// <summary>
+		/// Gets the parity type.
+		/// </summary>
+		public override eComParityType ParityType { get { return m_ParityType; } }
+
+		/// <summary>
+		/// Gets the number of stop bits.
+		/// </summary>
+		public override eComStopBits NumberOfStopBits { get { return m_NumberOfStopBits; } }
+
+		/// <summary>
+		/// Gets the protocol type.
+		/// </summary>
+		public override eComProtocolType ProtocolType { get { return m_ProtocolType; } }
+
+		/// <summary>
+		/// Gets the hardware handshake mode.
+		/// </summary>
+		public override eComHardwareHandshakeType HardwareHandShake { get { return m_HardwareHandShake; } }
+
+		/// <summary>
+		/// Gets the software handshake mode.
+		/// </summary>
+		public override eComSoftwareHandshakeType SoftwareHandshake { get { return m_SoftwareHandshake; } }
+
+		/// <summary>
+		/// Gets the report CTS changes mode.
+		/// </summary>
+		public override bool ReportCtsChanges { get { return m_ReportCtsChanges; } }
+
+		#endregion
 
 		/// <summary>
 		/// Constructor.
@@ -56,6 +109,14 @@ namespace ICD.Connect.Protocol.Mock.Ports.ComPort
 		/// <param name="comSpec"></param>
 		public override void SetComPortSpec(ComSpec comSpec)
 		{
+			m_BaudRate = comSpec.BaudRate;
+			m_NumberOfDataBits = comSpec.NumberOfDataBits;
+			m_ParityType = comSpec.ParityType;
+			m_NumberOfStopBits = comSpec.NumberOfStopBits;
+			m_ProtocolType = comSpec.ProtocolType;
+			m_HardwareHandShake = comSpec.HardwareHandShake;
+			m_SoftwareHandshake = comSpec.SoftwareHandshake;
+			m_ReportCtsChanges = comSpec.ReportCtsChanges;
 		}
 	}
 }
