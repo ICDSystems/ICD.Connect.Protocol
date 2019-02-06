1c23312d617a9e2f48ca8848e490d2213917cb03
Chris Cameron <chris.cameron@wedoresi.com>
2019-02-06 14:46:12 -0500

    feat: Initial commit of AsbtractReply

---
+++ ICD.Connect.Protocol.Network/Direct/AbstractReply.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ICD.Connect.Protocol.Network.Direct
+{
+	public abstract class AbstractReply : AbstractMessage, IReply
+	{
+		/// <summary>
+		/// The ID of the initial message that is being replied to.
+		/// </summary>
+		[JsonProperty("oi", DefaultValueHandling = DefaultValueHandling.Ignore)]
+		public Guid OriginalMessageId { get; set; }
+	}
+}
