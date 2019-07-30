fdfe88d76d6a94858e0b6364b1a8ae9a5d5abdff
Laura Gomez <laura.gomez@profound-tech.com>
2019-07-30 12:19:03 -0400

    feat: Initial commit of JwtUtils class for signing JWT payloads

---
+++ ICD.Connect.Protocol.Network/Utils/JwtUtils.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+
+namespace ICD.Connect.Protocol.Network.Utils
+{
+	public static class JwtUtils
+	{
+		public static string SignRs256(string payload, string privateKey)
+		{
+			List<string> segments = new List<string>();
+
+			// Encode the header
+			const string header = @"{""alg"":""RS256"",""typ"":""JWT""}";
+			byte[] headerBytes = Encoding.UTF8.GetBytes(header);
+			segments.Add(Base64UrlEncode(headerBytes));
+
+			// Encode the payload
+			byte[] payloadBytes = Encoding.UTF8.GetBytes(payload);
+			segments.Add(Base64UrlEncode(payloadBytes));
+
+			// Get the header.payload for signature hash
+			string stringToSign = string.Join(".", segments.ToArray());
+			byte[] bytesToSign = Encoding.UTF8.GetBytes(stringToSign);
+
+			// Build the signature
+			byte[] keyBytes = Convert.FromBase64String(privateKey);
+
+			AsymmetricKeyParameter asymmetricKeyParameter = PrivateKeyFactory.CreateKey(keyBytes);
+			RsaKeyParameters rsaKeyParameter = (RsaKeyParameters)asymmetricKeyParameter;
+			ISigner sig = SignerUtilities.GetSigner("SHA256withRSA");
+			sig.Init(true, rsaKeyParameter);
+
+			sig.BlockUpdate(bytesToSign, 0, bytesToSign.Length);
+			byte[] signature = sig.GenerateSignature();
+
+			segments.Add(Base64UrlEncode(signature));
+			return string.Join(".", segments.ToArray());
+		}
+
+		// from JWT spec
+		private static string Base64UrlEncode(byte[] input)
+		{
+			string output = Convert.ToBase64String(input);
+			output = output.Split('=')[0]; // Remove any trailing '='s
+			output = output.Replace('+', '-'); // 62nd char of encoding
+			output = output.Replace('/', '_'); // 63rd char of encoding
+			return output;
+		}
+	}
+}
