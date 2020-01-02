434717f44fe7218afd5e6bd3b4eb67b763f99f3b
Laura Gomez <laura.gomez@profound-tech.com>
2020-01-02 16:24:56 -0500

    feat: Added WebPortResponse to help IWebPort return byte arrays and headers instad of just strings.

---
+++ ICD.Connect.Protocol.Network/Ports/Web/WebPortResponse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace ICD.Connect.Protocol.Network.Ports.Web
+{
+	public sealed class WebPortResponse
+	{
+		public static WebPortResponse Failed { get { return new WebPortResponse(); } }
+
+		public bool Success { get; set; }
+		public int StatusCode { get; set; }
+		public byte[] Data { get; set; }
+		public IDictionary<string, string[]> Headers { get; set; }
+
+		/// <summary>
+		/// Converts the byte array to an extended-ASCII string for backwards compatibility.
+		/// </summary>
+		public string DataAsString
+		{
+			get { return Data == null ? null : Encoding.GetEncoding(28591).GetString(Data, 0, Data.Length); }
+		}
+
+		public override string ToString()
+		{
+			return DataAsString ?? "NULL";
+		}
+	}
+}
