4270d71476dbc8013edfc811ea3bbb9ab5e19cdb
Chris Cameron <chris.cameron@wedoresi.com>
2020-02-14 14:45:33 -0500

    chore: Updating changelog, incrementing minor version

--- ICD.Connect.Protocol.Crosspoints/Properties/AssemblyInfo.cs
+++ ICD.Connect.Protocol.Crosspoints/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Protocol.Crosspoints")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Protocol.Crosspoints")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
-[assembly: AssemblyVersion("7.6.0.0")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
+[assembly: AssemblyVersion("7.7.0.0")]
