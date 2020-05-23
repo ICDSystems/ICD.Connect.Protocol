4b2a8ae3aabbcf5ab005a7d24d9c362236db6fec
Chris Cameron <chris.cameron@icdsystems.com>
2020-05-22 23:31:06 -0400

    fix: Serial buffers create worker threads, fixes stability issues with TCP

--- ICD.Connect.Protocol/SerialBuffers/AbstractSerialBuffer.cs
+++ ICD.Connect.Protocol/SerialBuffers/AbstractSerialBuffer.cs
@@ -36,7 +36,7 @@ namespace ICD.Connect.Protocol.SerialBuffers
 		public void Enqueue(string data)
 		{
 			m_QueueSection.Execute(() => m_Queue.Enqueue(data));
-			Parse();
+			ThreadingUtils.SafeInvoke(Parse);
 		}
 
 		/// <summary>
