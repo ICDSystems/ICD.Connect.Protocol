c74b2aa22bf3b8e3a18ed95ddda889663467e5bd
Chris Cameron <chris.cameron@wedoresi.com>
2021-01-06 16:45:05 -0500

    chore: Updating copyright year

--- ICD.Connect.Protocol.Network/Properties/AssemblyInfo.cs
+++ ICD.Connect.Protocol.Network/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Protocol.Network")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Protocol.Network")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
 [assembly: AssemblyVersion("13.3.3.0")]
