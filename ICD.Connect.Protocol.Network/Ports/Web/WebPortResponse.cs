4bc05867aa28cdf3695eba9b50a28aae31e39149
Chris Cameron <chris.cameron@icdsystems.com>
2022-01-05 15:41:02 -0500

    perf: Cache web response string data

--- ICD.Connect.Protocol.Network/Ports/Web/WebPortResponse.cs
+++ ICD.Connect.Protocol.Network/Ports/Web/WebPortResponse.cs
@@ -5,21 +5,32 @@ namespace ICD.Connect.Protocol.Network.Ports.Web
 {
 	public sealed class WebPortResponse
 	{
+		private string m_DataAsString;
+
 		public static WebPortResponse Failed { get { return new WebPortResponse(); } }
 
 		public bool GotResponse { get; set; }
 		public int StatusCode { get; set; }
-		public bool IsSuccessCode { get { return StatusCode < 300; } }
 		public byte[] Data { get; set; }
 		public IDictionary<string, string[]> Headers { get; set; }
 		public string ResponseUrl { get; set; }
+
+		public bool IsSuccessCode { get { return StatusCode < 300; } }
 
 		/// <summary>
 		/// Converts the byte array to an extended-ASCII string for backwards compatibility.
 		/// </summary>
 		public string DataAsString
 		{
-			get { return Data == null ? null : Encoding.GetEncoding(28591).GetString(Data, 0, Data.Length); }
+			get
+			{
+				if (m_DataAsString == null)
+				{
+					m_DataAsString =
+						Data == null ? null : Encoding.GetEncoding(28591).GetString(Data, 0, Data.Length);
+				}
+				return m_DataAsString;
+			}
 		}
 	}
 }
