494188942e43032aff7b77188367dfdc60a3ccb3
Drew Tingen <drew.tingen@icdsystems.com>
2023-02-06 16:48:04 -0500

    feat(MockIoPort): Set Input status via console commands

--- ICD.Connect.Protocol.Mock/Ports/IoPort/MockIoPort.cs
+++ ICD.Connect.Protocol.Mock/Ports/IoPort/MockIoPort.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using ICD.Connect.API.Commands;
 using ICD.Connect.Protocol.Ports.IoPort;
 
 namespace ICD.Connect.Protocol.Mock.Ports.IoPort
@@ -33,5 +35,45 @@ namespace ICD.Connect.Protocol.Mock.Ports.IoPort
 
 			DigitalOut = digitalOut;
 		}
+
+		private void SetDigitalInput(bool state)
+		{
+			if (Configuration != eIoPortConfiguration.DigitalIn)
+				throw new InvalidOperationException("Not in digital input mode");
+
+			DigitalIn = state;
+		}
+
+		private void SetAnalogInput(ushort value)
+		{
+			if (Configuration != eIoPortConfiguration.AnalogIn)
+				throw new InvalidOperationException("Not in analog input mode");
+			
+			AnalogIn = value;
+		}
+		
+		#region Console
+
+		/// <summary>
+		/// Gets the child console commands.
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<IConsoleCommand> GetConsoleCommands()
+		{
+			foreach (IConsoleCommand command in GetBaseConsoleCommands())
+				yield return command;
+
+			yield return new GenericConsoleCommand<bool>("SetDigitalIn", "SetDigitalIn (bool) - Sets the state of the digital input, true|false",
+				state => SetDigitalInput(state));
+			yield return new GenericConsoleCommand<ushort>("SetAnalogIn",
+				"SetAnalogIn (ushort) - Sets the value of the analog input, 0 - 65535", value => SetAnalogInput(value));
+		}
+
+		private IEnumerable<IConsoleCommand> GetBaseConsoleCommands()
+		{
+			return base.GetConsoleCommands();
+		}
+
+		#endregion
 	}
 }
